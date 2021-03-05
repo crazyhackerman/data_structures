333a19c86589e9a506d4456e3548f096bbf98a56
Utkarsh Chauhan <26863711+crazyhackerman@users.noreply.github.com>
2021-03-05 13:27:38 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
+#define SIZE 4
 using namespace std;
 int main() {
-    int queue[100],x,j,front=1,rear=0,choice=1;
+    int queue[SIZE],x,j,front=0,rear=0,choice=1;;
     while(1){
         cout<<"\n1.enqueue, 2.dequeue, 3.front, 4.rear 5.print 0.exit\n";
         cin>>choice;
         switch(choice){
-            case 1: cout<<"enqueue:";
-                    cin>>x;
-                    rear++;
-                    queue[rear] = x;
+            case 1: if(rear==SIZE){
+                        cout<<"Queue is full";
+                    }
+                    else{
+                        cout<<"enqueue:";
+                        cin>>x;
+                        queue[rear] = x;
+                        rear++;
+                    }
                     break;
-            case 2: if(front>=1){
+            case 2: if(front!=rear){
                         cout<<"dequeue:"<<queue[front]<<"\n";
                         front++;
+                        if(front==SIZE && rear==SIZE){
+                            front=0;
+                            rear=0;
+                        }
                     }
                     else
                         cout<<"queue is empty\n";
                     break;
-            case 3: if(front>=1)
+            case 3: if(front!=rear)
                         cout<<"front:"<<queue[front]<<"\n";
                     else
                         cout<<"queue is empty\n";
                     break;
-            case 4: if(front>=1)
-                        cout<<"rear:"<<queue[rear]<<"\n";
+            case 4: if(front!=rear)
+                        cout<<"rear:"<<queue[rear-1]<<"\n";
                     else
                         cout<<"queue is empty\n";
                     break;
             case 5: cout<<"queue:";
-                    if(front>=1){
-                        for(j=front;j<=rear;j++)
+                    if(front!=rear){
+                        for(j=front;j<=rear-1;j++)
                             cout<<queue[j]<<",";
                     }
                     else
